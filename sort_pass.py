62f8f3737bff41754f08573fc48b894877cc55f2
Doug <mcgidoug@gmail.com>
2023-11-10 13:34:44 -0600

    completed sort_pass function

--- sort_pass.py
+++ sort_pass.py
@@ -1,2 +1,14 @@
+# todo
 # write a func that sorts through an array of passwords and returns the ones containing a certain char
 
+arr = ["word", "dog", "hat", "hello", "wash", "text", "flock"]
+
+def pull_words(letter):
+    new_arr = []
+    for word in arr:
+        if letter in word:
+            new_arr.append(word)
+    print(new_arr)
+
+
+pull_words("o")
