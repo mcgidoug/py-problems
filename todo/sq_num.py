6dbaa1236caffadbda9cc450bf2bb4c821b3a347
Doug <mcgidoug@gmail.com>
2023-11-29 14:26:26 -0600

    added sq_num problem in todo dir

---
+++ todo/sq_num.py
@@ -0,0 +1,13 @@
+def square_number(num):
+    # Your code here
+    pass  # Remove this line when you write your code
+
+# Test cases
+print(square_number(4))  # Expected output: 16
+print(square_number(7))  # Expected output: 49
+print(square_number(10))  # Expected output: 100
+
+
+# Write a function called square_number that takes a single argument num and returns the square of that number.
+
+# Your task is to implement the square_number function so that it correctly calculates and returns the square of the given number. Test your function with different values to ensure it works as expected!
