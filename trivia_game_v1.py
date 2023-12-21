e85a897d9519646b6858bfb70358d5a6e79a7ae8
Doug <mcgidoug@gmail.com>
2023-12-21 12:32:34 -0600

    re-added original version reformatted

--- trivia_game_v1.py
+++ trivia_game_v1.py
@@ -15,10 +15,11 @@ def switchframe():
     next.destroy()
 
 
-q1 = ttk.Frame(win, padding=10, width=500, height=500)
-ttk.Label(frm, text="welcome to trivia night").grid(column=1, row=0)
 next = ttk.Button(win, text="next", command=switchframe)
 next.pack()
+ttk.Label(frm, text="welcome to trivia night").grid(column=1, row=0)
+
+q1 = ttk.Frame(win, padding=10, width=500, height=500)
 
 ttk.Label(
     q1, text="How old was Queen Elizabeth II when she was crowned Queen of England?"
@@ -27,9 +28,13 @@ ttk.Label(q1, text="this question is worth 5 points").pack(side=TOP)
 
 answer1 = StringVar(q1, "27")
 Radiobutton(q1, text="27", variable=answer1, value="27").pack(side=LEFT)
+
 Radiobutton(q1, text="25", variable=answer1, value="25").pack(side=LEFT)
+
 Radiobutton(q1, text="22", variable=answer1, value="22").pack(side=LEFT)
+
 Radiobutton(q1, text="28", variable=answer1, value="28").pack(side=LEFT)
+# next question
 
 q2 = ttk.Frame(win, padding=10, width=500, height=500)
 
@@ -42,13 +47,17 @@ def switchframe1():
 
 next1 = ttk.Button(q1, text="next", command=switchframe1)
 next1.pack(side=BOTTOM)
+
 ttk.Label(q2, text="What is the name of Thomas Jefferson's main house?").pack(side=TOP)
 ttk.Label(q2, text="This question is worth 3 points ").pack(side=TOP)
 
 answer2 = StringVar(q2, "monticello")
 Radiobutton(q2, text="monticello", variable=answer2, value="monticello").pack(side=LEFT)
+
 Radiobutton(q2, text="cedar", variable=answer2, value="cedar").pack(side=LEFT)
+
 Radiobutton(q2, text="vernon", variable=answer2, value="vernon").pack(side=LEFT)
+
 Radiobutton(q2, text="greentown", variable=answer2, value="greentown").pack(side=LEFT)
 
 q3 = ttk.Frame(win, padding=10, width=500, height=500)
@@ -60,26 +69,31 @@ def switchframe2():
     next2.destroy()
 
 
-next2 = ttk.Button(q2, text="next", command=switchframe2)
-next2.pack(side=BOTTOM)
+next2 = ttk.Button(q2, text="next", command=switchframe1)
+
 ttk.Label(q3, text="What year was the wreck of the Titanic discovered?").pack(side=TOP)
+
+# next2= ttk.Button (win, text="next", command=switchframe1)
 
 answer3 = StringVar(q3, "1985")
 Radiobutton(q3, text="1981", variable=answer2, value="1981").pack(side=LEFT)
 Radiobutton(q3, text="1978", variable=answer2, value="1978").pack(side=LEFT)
+
 Radiobutton(q3, text="1985", variable=answer2, value="1985").pack(side=LEFT)
+
 Radiobutton(q3, text="1992", variable=answer2, value="1992").pack(side=LEFT)
 
-points = 0
 
 def answer_count():
     global points
-    if answer1.get() == "25":
+    points = 0
+    if answer1.get() == "27":
         points += 5
         ttk.Label(
             q2, text="Your previous answer was Correct! Your score is " + str(points)
         ).pack(side=TOP)
     else:
+        # print("wrong")
         ttk.Label(
             q2, text="Your previous answer was Incorrect! Your score is " + str(points)
         ).pack(side=TOP)
